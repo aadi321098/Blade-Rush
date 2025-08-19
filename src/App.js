6103e5d401897be722a5046b82239426867a9960
aadi321098 <aadi321098@gmail.com>
2025-08-19 15:34:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import FooterNav from './components/FooterNav';
 import AudioPlayer from './components/AudioPlayer';
-import LevelSelectPage from './pages/LevelSelectPage';
-
-import PremiumPage from "./pages/PremiumPage";
-
-
-
 
 import Home from './pages/Home/Home';
 import Settings from './pages/Settings/Settings';
@@ -24,24 +18,23 @@ import Privacy from './pages/Legal/Privacy';
 import Terms from './pages/Legal/Terms';
 import Help from './pages/Legal/help';
 
-
+import PremiumPage from "./pages/PremiumPage";
 import Tournament from './pages/Tournament';
 import TournamentLeaderboard from './pages/TournamentLeaderboard'; 
 
-// ✅ Import the Knife Game page wrapper
+// âœ… Import the Knife Game page wrapper
 import KnifePage from './KnifePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* ✅ Knife route without header/footer/audio */}
+        {/* âœ… Knife route without header/footer/audio */}
         <Route path="/rush" element={<RushMode />} />
         
-        <Route path="/knife" element={<KnifePage />} />
-  <Route path="/select-level" element={<LevelSelectPage />} /> {/* ✅ New route */}
+        <Route path="/level" element={<KnifePage />} />
 
-        {/* ✅ All other pages with layout */}
+        {/* âœ… All other pages with layout */}
         <Route
           path="*"
           element={
@@ -65,10 +58,9 @@ function App() {
                 <Route path="/terms" element={<Terms />} />
                 
                 <Route path="/help" element={<Help />} />
+               <Route path="/premium" element={<PremiumPage />} />
                 
                  <Route path="/tournament" element={<Tournament />} />
-                 <Route path="/premium" element={<PremiumPage />} />
-                 
   <Route path="/tournament-leaderboard" element={<TournamentLeaderboard />} />
   
               </Routes>
@@ -77,7 +69,7 @@ function App() {
           }
         />
 
-        {/* ❌ RushMode without layout */}
+        {/* âŒ RushMode without layout */}
         
       </Routes>
     </Router>
