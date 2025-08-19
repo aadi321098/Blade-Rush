fd56cc19eb830ca32eeb8f85480501f0829a33b4
aadi321098 <aadi321098@gmail.com>
2025-08-19 15:43:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 export const API_BASE = "https://try2back.onrender.com"; 
 export const APP_ID   = "67eff0ae0199aac7a37cf903";
-export const SANDBOX  = false;
+export const SANDBOX  = true;
 
 // src/configScore.js
 export const SCORE_API = "http://localhost:6000";
